fa4d10958eeec818c9ce7622014f5f5f04b46eb9
Emilie LSM <99273880+emilie06lsm@users.noreply.github.com>
2022-03-22 15:05:12 +0000

    edit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// Copyright (c) 2022 Emilie All rights reserved
+//
+// Created by: Emilie
+// Created on: Mar 2022
+// This file contains the JS functions for index.html
+
+function calculateArea() { 
+  //area and perimeter
+  document.getElementById('area').innerHTML = 'Area of the rectangle = ' + (5 * 4)
+}
+function calculateperimeter() {  
+  document.getElementById('perimeter').innerHTML = 'Perimeter of the rectangle =  ' + (5 + 5 + 4 + 4)
+}
